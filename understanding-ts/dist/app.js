c712de5453bac330817d98892c745030714650e3
karnkumar.git@gmail.com <karnkumar.git@gmail.com>
2022-08-29 19:47:56 +0530

    interface complete

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,21 +1,5 @@
 "use strict";
-class Department {
-    constructor(name, state) {
-        this.name = name;
-        this.state = state;
-    }
-    static getInstance(deptname, state) {
-        if (Department.instance) {
-            return this.instance;
-        }
-        else {
-            this.instance = new Department(deptname, state);
-            return this.instance;
-        }
-    }
-}
-const d1 = Department.getInstance('d1', 'Bihar');
-const d2 = Department.getInstance('d2', 'Delhi');
-console.log(d1);
-console.log(d2);
+let user = {
+    name: 'Karan'
+};
 //# sourceMappingURL=app.js.map
