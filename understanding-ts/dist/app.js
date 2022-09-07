0a364ecb15d43e5037a06f1c4a8f54566e17f4aa
karnkumar.git@gmail.com <karnkumar.git@gmail.com>
2022-09-07 18:59:29 +0530

    generics day 1

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,5 +1,7 @@
 "use strict";
-const userInput = null;
-const storedData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storedData);
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
+}
+const ans1 = merge({ name: 'ramesh' }, { age: 23 });
+console.log(ans1);
 //# sourceMappingURL=app.js.map
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,3 +1,7 @@
-const userInput = null;
-const storedData = userInput ?? 'DEFAULT';
-console.log(storedData);
+function merge<T extends object, U extends object>(obj1: T, obj2: U){
+    return Object.assign(obj1, obj2);
+}
+
+const ans1 = merge({name: 'ramesh'}, {age: 23})
+console.log(ans1);
+
