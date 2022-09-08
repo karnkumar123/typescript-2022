667e9477ce11188ba3891d659f05fd8e9d393be2
karnkumar.git@gmail.com <karnkumar.git@gmail.com>
2022-09-08 16:21:13 +0530

    generics day2

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,7 +1,26 @@
 "use strict";
-function merge(obj1, obj2) {
-    return Object.assign(obj1, obj2);
+class DataStorage {
+    constructor(listofItems) {
+        this.listOfItems = listofItems;
+    }
+    addItem(item) {
+        this.listOfItems.push(item);
+    }
+    removeItem(item) {
+        this.listOfItems.splice(this.listOfItems.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.listOfItems];
+    }
 }
-const ans1 = merge({ name: 'ramesh' }, { age: 23 });
-console.log(ans1);
+let numberDataStorage = new DataStorage([1, 2]);
+numberDataStorage.addItem(3);
+numberDataStorage.addItem(4);
+numberDataStorage.removeItem(3);
+console.log(numberDataStorage.getItems());
+let stringDataStorage = new DataStorage(['Amar']);
+stringDataStorage.addItem('Akbar');
+stringDataStorage.addItem('Anthony');
+stringDataStorage.removeItem('Akbar');
+console.log(stringDataStorage.getItems());
 //# sourceMappingURL=app.js.map
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,7 +1,31 @@
-function merge<T extends object, U extends object>(obj1: T, obj2: U){
-    return Object.assign(obj1, obj2);
+class DataStorage<T extends number | string | boolean>{
+    private listOfItems;
+
+    constructor(listofItems: T[]){
+        this.listOfItems = listofItems;
+    }
+
+    addItem(item: T){
+        this.listOfItems.push(item);
+    }
+
+    removeItem(item: T){
+        this.listOfItems.splice(this.listOfItems.indexOf(item),1);
+    }
+
+    getItems(): T[]{
+        return [...this.listOfItems];
+    }
 }
 
-const ans1 = merge({name: 'ramesh'}, {age: 23})
-console.log(ans1);
+let numberDataStorage = new DataStorage<number>([1,2]);
+numberDataStorage.addItem(3);
+numberDataStorage.addItem(4);
+numberDataStorage.removeItem(3);
+console.log(numberDataStorage.getItems());
 
+let stringDataStorage = new DataStorage<string>(['Amar']);
+stringDataStorage.addItem('Akbar');
+stringDataStorage.addItem('Anthony');
+stringDataStorage.removeItem('Akbar');
+console.log(stringDataStorage.getItems());
