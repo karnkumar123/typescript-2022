61213be47a9a3379a8da27c323be8ce6ce1a7636
karnkumar.git@gmail.com <karnkumar.git@gmail.com>
2022-10-03 14:55:18 +0530

    more generic content

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,26 +1,6 @@
 "use strict";
-class DataStorage {
-    constructor(listofItems) {
-        this.listOfItems = listofItems;
-    }
-    addItem(item) {
-        this.listOfItems.push(item);
-    }
-    removeItem(item) {
-        this.listOfItems.splice(this.listOfItems.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.listOfItems];
-    }
+function getLength(num) {
+    return Math.pow(num, 2);
 }
-let numberDataStorage = new DataStorage([1, 2]);
-numberDataStorage.addItem(3);
-numberDataStorage.addItem(4);
-numberDataStorage.removeItem(3);
-console.log(numberDataStorage.getItems());
-let stringDataStorage = new DataStorage(['Amar']);
-stringDataStorage.addItem('Akbar');
-stringDataStorage.addItem('Anthony');
-stringDataStorage.removeItem('Akbar');
-console.log(stringDataStorage.getItems());
+console.log(getLength(3));
 //# sourceMappingURL=app.js.map
---
+++ understanding-ts/src/app.ts
@@ -0,0 +1,7 @@
+function getLength<T extends number>(num: T){
+   return Math.pow(num, 2);
+}
+console.log(getLength(3));
+
+
+
